2f6b5582211aa7695ebbcc5d0565ae46bfe6a423
Cristian Madrid <cristianmadridd@outlook.com>
2023-07-04 21:48:46 -0300

    chore: create the basic structure

---
+++ AmbushExperiment/AmbushExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbushExperimentApp.swift
+//  AmbushExperiment
+//
+//  Created by Cristian Madrid on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AmbushExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmbushExperimentUITests/AmbushExperimentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AmbushExperimentUITestsLaunchTests.swift
+//  AmbushExperimentUITests
+//
+//  Created by Cristian Madrid on 04/07/23.
+//
+
+import XCTest
+
+final class AmbushExperimentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
